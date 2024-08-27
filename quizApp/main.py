b1a6e1956133b1ebd298a3deba155b5c4e23d2de
revolutioncs <obhardwaj169@gmail.com>
2024-08-27 20:10:37 +0530

    Initial Commit

---
+++ quizApp/main.py
@@ -0,0 +1,34 @@
+quizes = [
+    {
+        id: 1,
+        "quiz": "What is the full form of UPSC",
+        "ans": "Union Public Service Commission",
+        "options": ["Union People Server Commission", "Union Public Service Commission", " Union Public Servants Commission"]
+    },
+    {
+        id: 2,
+        "quiz": "How many continents are there?",
+        "ans": 7,
+        "options": [5, 10, 9]
+    },
+    {
+        id: 3,
+        "quiz": "What does USA stands for?",
+        "ans": "United states of America",
+        "options": ["Universal Statistics Association", "United States of Armenia", "United states of America"]
+    },
+    {
+        id: 4,
+        "quiz": "What is the capital of India?",
+        "ans": "New Delhi",
+        "options": ["Mumabi", "Rajasthan", "New Delhi"]
+    },
+    {
+        id: 5,
+        "quiz": "What is the biggest animal on Earth",
+        "ans": "Blue Whale",
+        "options": ["Girrafe", "Elephant", "Blue Whale"]
+    },
+]
+
+
