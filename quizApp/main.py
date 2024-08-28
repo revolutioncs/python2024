9815c5228438bb7f9b521d113dfbe749fafb1658
revolutioncs <obhardwaj169@gmail.com>
2024-08-28 17:32:27 +0530

    Made quizes array

--- quizApp/main.py
+++ quizApp/main.py
@@ -1,34 +1,14 @@
-quizes = [
-    {
-        id: 1,
-        "quiz": "What is the full form of UPSC",
-        "ans": "Union Public Service Commission",
-        "options": ["Union People Server Commission", "Union Public Service Commission", " Union Public Servants Commission"]
+quizes=[
+    {  
+        "question":"who is the best cs teacher",
+        "answer":"abhinav",
+        "options":["abhinav","raj","raghav","arun"]
     },
     {
-        id: 2,
-        "quiz": "How many continents are there?",
-        "ans": 7,
-        "options": [5, 10, 9]
-    },
-    {
-        id: 3,
-        "quiz": "What does USA stands for?",
-        "ans": "United states of America",
-        "options": ["Universal Statistics Association", "United States of Armenia", "United states of America"]
-    },
-    {
-        id: 4,
-        "quiz": "What is the capital of India?",
-        "ans": "New Delhi",
-        "options": ["Mumabi", "Rajasthan", "New Delhi"]
-    },
-    {
-        id: 5,
-        "quiz": "What is the biggest animal on Earth",
-        "ans": "Blue Whale",
-        "options": ["Girrafe", "Elephant", "Blue Whale"]
-    },
+        "question":"capital of india",
+        "answer":"new delhi",
+        "options":["lucknow","punjab","rajasthan","new delhi"]
+    }
 ]
 
-
+print(quizes[1])
