251904ae7cac7076d3cc70bffe73caba35ffe834
revolutioncs <obhardwaj169@gmail.com>
2024-08-29 19:50:43 +0530

    Done creating quiz game

--- quizApp/main.py
+++ quizApp/main.py
@@ -1,14 +1,44 @@
 quizes=[
     {  
-        "question":"who is the best cs teacher",
-        "answer":"abhinav",
-        "options":["abhinav","raj","raghav","arun"]
+        "question":"who is the cs teacher?",
+        "answer":"abhinav sir",
+        "options":["abhinav sir","raj sir","raghav sir","arun sir"]
     },
     {
-        "question":"capital of india",
-        "answer":"new delhi",
-        "options":["lucknow","punjab","rajasthan","new delhi"]
-    }
+        "question":"who is the bio teacher?",
+        "answer":"raj sir",
+        "options":["abhinav sir","raj sir","raghav sir","arun sir"]
+    },
+    {
+        "question":"who is the maths teacher?",
+        "answer":"raghav sir",
+        "option":["abhinav sir","raj sir","raghav sir","arun sir"]
+    },
+    {
+        "question":"who is the physics teacher?",
+        "answer":"arun sir",
+        "option":["abhinav sir","raj sir","raghav sir","arun sir"]
+    },
+    
+    
 ]
 
-print(quizes[1])
+
+points = 0
+failedStreak = 0
+
+
+for i in quizes:
+    if(failedStreak != 2):
+        print(i["question"])
+        x = input('Enter answer: ')
+        if(x == i["answer"]):
+            points+=10
+            print("Answer is correct\n", points)
+            failedStreak=0
+        else:
+            print("Answer is incorrect")
+            failedStreak+=1
+    else:
+        print("You failed")
+        break
